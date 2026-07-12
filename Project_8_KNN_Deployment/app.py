bf35c6bff9ab459797e0579427cd4b0f2f646e9c
Anamikaa200 <yanamika029@gmail.com>
2026-07-12 13:22:15 +0530

    Update app.py

--- Project_8_KNN_Deployment/app.py
+++ Project_8_KNN_Deployment/app.py
@@ -136,11 +136,11 @@ st.write("""
 
 st.divider()
 
-st.markdown("### 👨‍💻 Developed by Abhay Kumar Gupta")
+st.markdown("### 👨‍💻 Developed by Anamika Yadav")
 
 st.markdown(
 """
-- 🔗 **GitHub:** https://github.com/Abhay-cody
-- 💼 **LinkedIn:** https://www.linkedin.com/in/abhay-kumar-gupta-104a18397
+- 🔗 **GitHub:** https://github.com/Anamikaa200
+- 💼 **LinkedIn:** www.linkedin.com/in/anamika-yadav-64b688340
 """
 )
